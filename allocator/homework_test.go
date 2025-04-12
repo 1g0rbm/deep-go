e69dc4d027f037ad9b7edecc433a0660da515ccc
1g0rbm <m1g0rb89@gmail.com>
2025-04-12 21:01:29 +0300

    Resolved additional task.

--- allocator/homework_test.go
+++ allocator/homework_test.go
@@ -10,23 +10,33 @@ import (
 
 // go test -v homework_test.go
 
-func Defragment(memory []byte, pointers []unsafe.Pointer) {
+func Defragment(memory []byte, pointers []unsafe.Pointer, size int) {
+	if size != 1 && size != 2 && size != 4 && size != 8 {
+		panic("size must be 1, 2, 4, or 8")
+	}
+
 	ptrsMap := make(map[uintptr]int)
 	for i, pointer := range pointers {
 		ptrsMap[uintptr(pointer)] = i
 	}
 
 	writeIdx := 0
-	for i := 0; i < len(memory); i++ {
+	for i := 0; i < len(memory); {
 		pointer := uintptr(unsafe.Pointer(&memory[i]))
 		if idx, exists := ptrsMap[pointer]; exists {
 			if writeIdx != i {
-				memory[writeIdx] = memory[i]
-				memory[i] = 0
+				copy(memory[writeIdx:writeIdx+size], memory[i:i+size])
 			}
 			pointers[idx] = unsafe.Pointer(&memory[writeIdx])
-			writeIdx++
+			writeIdx += size
+			i += size
+		} else {
+			i += 1
 		}
+	}
+
+	for i := writeIdx; i < len(memory); i++ {
+		memory[i] = 0
 	}
 }
 
@@ -59,7 +69,69 @@ func TestDefragmentation(t *testing.T) {
 		0x00, 0x00, 0x00, 0x00,
 	}
 
-	Defragment(fragmentedMemory, fragmentedPointers)
+	Defragment(fragmentedMemory, fragmentedPointers, 1)
+
+	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
+	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
+}
+
+func TestDefragmentation2bytes(t *testing.T) {
+	var fragmentedMemory = []byte{
+		0xFF, 0xFF, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	var fragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[10]),
+	}
+
+	var defragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[2]),
+	}
+
+	var defragmentedMemory = []byte{
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	Defragment(fragmentedMemory, fragmentedPointers, 2)
+
+	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
+	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
+}
+
+func TestDefragmentation4bytes(t *testing.T) {
+	var fragmentedMemory = []byte{
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0xFF, 0xFF, 0xFF, 0xFF,
+	}
+
+	var fragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[12]),
+	}
+
+	var defragmentedPointers = []unsafe.Pointer{
+		unsafe.Pointer(&fragmentedMemory[0]),
+		unsafe.Pointer(&fragmentedMemory[4]),
+	}
+
+	var defragmentedMemory = []byte{
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	Defragment(fragmentedMemory, fragmentedPointers, 4)
 
 	assert.True(t, reflect.DeepEqual(defragmentedMemory, fragmentedMemory))
 	assert.True(t, reflect.DeepEqual(defragmentedPointers, fragmentedPointers))
